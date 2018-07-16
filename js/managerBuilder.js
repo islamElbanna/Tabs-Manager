8ccdd6a63c0b6d7a0fbfbc11249042f6cc172036
Islam Elbanna <islam.elbanna@badrit.com>
2018-07-17 00:19:25 +0200

    Add options button

--- js/managerBuilder.js
+++ js/managerBuilder.js
@@ -268,3 +268,11 @@ function addGlobalEvents(){
 }
 
 load();
+
+$('#go-to-options').click(function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
